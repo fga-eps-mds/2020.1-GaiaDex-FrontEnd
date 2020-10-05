3e9c9246696c4a27717be3b1b8adec0352f55cc6
V100K <vitao.hugo@hotmail.com>
2020-10-05 05:58:52 -0300

    Registration Screen (FrontEnd) Created

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { Platform, StyleSheet, Text, View, StatusBar as sBar} from 'react-native';
+import Regform from './app/components/Regform';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Regform/>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,9 +15,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#2d5f00',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingLeft: 60,
+    paddingRight: 60,
     paddingTop: Platform.OS === "android" ? sBar.currentHeight : 0,
   },
 });
