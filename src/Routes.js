ba9867a7ce62745d238aea1c4d512733a4ed97cc
Rafael Makaha <rafael.makaha@gmail.com>
2020-11-03 22:40:18 -0300

    Add Router to project root
    Co-authored-by: Marcos Filipe <marofelipe@live.com>
    Co-authored-by: Antonio Ruan <ruanmoura13@outlook.com>

---
+++ src/Routes.js
@@ -0,0 +1,35 @@
+import {createAppContainer, createSwitchNavigator} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+
+import Scanner from './screen/scanner';
+import PlantView from './screen/plant/plantView';
+
+const AppStack = createStackNavigator(
+  { 
+    Plant: {
+      screen: PlantView,
+      path: 'Plant',
+      navigationOptions: {
+        headerShown: false
+      }
+    },
+    Scanner: {
+      screen: Scanner,
+      path: 'Scanner',
+      navigationOptions: {
+        headerShown: false
+      }
+    },
+  }
+)
+
+const Routes = createSwitchNavigator(
+  {
+    AppStack
+  },
+  {
+    initialRouteName: 'AppStack'
+  }
+)
+
+export default createAppContainer(Routes);
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Routes from './Routes';
 import PlantView from './screen/plant/plantView'
 import Scanner from './screen/scanner'
 
 
 export default function App() {
   return (
-    <Scanner />
+    <Routes />
   );
 }
