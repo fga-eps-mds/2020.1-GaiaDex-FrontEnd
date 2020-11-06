9c5136241d059514ef623014ad69e94d3a37ccab
Marcos Felipe <marofelipe@live.com>
2020-11-06 15:16:28 -0300

    Adding newComment and changes on pages's flow
    Co-authored-by: Rafael Makaha <rafael.makaha@gmail.com>
    Co-authored-by: Antonio Ruan <ruanmoura13@outlook.com>

--- src/screen/topic/header.js
+++ src/screen/topic/header.js
@@ -1,5 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
 import {
   View,
   Text,
@@ -46,51 +45,3 @@ const styles = StyleSheet.create({
   },
 });
 
-=======
-import { View, Text, StyleSheet, TouchableOpacity} from 'react-native';
-import { Dimensions } from 'react-native';
-import { MaterialCommunityIcons, AntDesign } from '@expo/vector-icons';
-
-const largura = Dimensions.get("screen").width;
-const altura = Dimensions.get("screen").height;
-
-export default function Header({isButton}){
-    return(
-        <View style={styles.header}>
-            <View style={{flex:1}}>
-                <AntDesign name="left" size={24} color="white" />
-            </View>
-            <View style={{flex:1}}>
-                <Text style={{color:"white",fontSize:18,alignSelf:'center'}}>Cenoura</Text>
-            </View>
-             <View style={{flex:1}}> 
-            </View>    
-        </View>
-    )
-};
-const styles = StyleSheet.create({
-    header: {
-        backgroundColor:'black',
-        flex:1.5,
-        flexDirection:'row',
-        alignItems:'center',
-        alignContent:'space-between',
-        paddingTop:15
-   
-    },
-    publicarDiv:{
-        flexDirection:'row',
-        alignItems:"center",
-        justifyContent:'center',
-        alignSelf:'center',
-    },
-    publicarText:{
-        marginRight:5,
-        color:'white',
-        fontSize:15,
-        fontWeight: '100',
-    },
-    backButton:{
-    }
-});
->>>>>>> 18c6322 (adding topic view)
