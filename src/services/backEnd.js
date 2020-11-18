4e4f7168d3cd5d08adaec16b9da60f67f48ada8d
Rafael Makaha <rafael.makaha@gmail.com>
2020-11-17 23:07:25 -0300

    Add registerPlant and scanner services
    Signed-off-by: Rafael Makaha <rafael.makaha@gmail.com>
    Co-authored-by: João Vítor Morandi <joao.lemos4000@gmail.com>
    Co-authored-by: Vinícius Vieira de Souza <vd.souza1999@gmail.com>
    Co-authored-by: Marcos Filipe <marofelipe@live.com>

--- src/services/backEnd.js
+++ src/services/backEnd.js
@@ -1,3 +1,4 @@
+import { __SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED } from 'react'
 import { ENDPOINTS } from '../settings'
 
 const baseHeaders = (token = '') => ({
@@ -10,8 +11,8 @@ export const createTopic = (topicID) => {
     const TOPIC = ENDPOINTS.API.topic
     return new Promise((resolve, reject) => {
         fetch(ENDPOINTS.API.base_url + TOPIC.create.route(topicID),{
-            method:TOPIC.create.method,
-            headers:baseHeaders(window.localStorage.getItem('token'))
+            method: TOPIC.create.method,
+            headers: baseHeaders(window.localStorage.getItem('token'))
         })
         .then(resolve)
         .then(reject)
@@ -23,6 +24,50 @@ export const getPlant = (plantID) => {
   return new Promise((resolve, reject) => {
     fetch(ENDPOINTS.API.base_url + PLANT.getPlant.route(plantID), {
       method: PLANT.getPlant.method
+    })
+    .then(res => res.json())
+    .then(resolve)
+    .catch(reject)
+  })
+}
+
+export const scannerPlant = (scannerBody) =>{
+  const SCANNER = ENDPOINTS.API.scanner;
+  console.log(scannerBody.filename)
+  return new Promise((resolve, reject) => {
+    fetch(
+      ENDPOINTS.API.base_url + SCANNER.route,
+      {
+        method: SCANNER.method,
+        headers: baseHeaders(),
+        body: JSON.stringify({
+          filename: scannerBody.filename,
+          data: scannerBody.data,
+          mime: scannerBody.mime,
+          plantType: scannerBody.plantType,
+        })
+      })
+      .then(res => res.json())
+      .then(resolve)
+      .then(reject)
+  })
+}
+
+export const registerPlant = (plant) => {
+  const PLANT = ENDPOINTS.API.plant;
+  return new Promise((resolve, reject) => {
+    fetch(
+      ENDPOINTS.API.base_url + PLANT.register, 
+      {
+        method: PLANT.register.method,
+        headers: baseHeaders(window.localStorage).getItem('token'),
+        body: {
+        scienctificName: plant.species.scientificNameWithoutAuthor,
+        genderName: plant.species.genus.scientificNameWithoutAuthor,
+        family_name: plant.species.family.scientificNameWithoutAuthor,
+        common_name: plant.commonNames[0],
+        gbifID: plant.gbifID,
+      }
     })
     .then(res => res.json())
     .then(resolve)
