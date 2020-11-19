69bc073c7971177fdac5ed84dff92e00e6169a4b
V100K <vitao.hugo@hotmail.com>
2020-11-19 17:21:06 -0300

    DeleteMyAccount button

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import Routes from './Routes';
 
 export default function App() {
-  return <Routes />;
+  return <Login />;
 }
 
--- src/screen/myplant/header.js
+++ src/screen/myplant/header.js
@@ -1,5 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
 import {
   View,
   Text,
@@ -54,48 +53,4 @@ const styles = StyleSheet.create({
     padding: 10,
   },
 });
-=======
-import { View, Text, StyleSheet, TouchableOpacity} from 'react-native';
-import { Dimensions } from 'react-native';
-import { MaterialIcons, AntDesign } from '@expo/vector-icons';
 
-const largura = Dimensions.get("screen").width;
-const altura = Dimensions.get("screen").height;
-
-export default function Header(){
-    return(
-            <View style={styles.header}>
-                <AntDesign name="left" size={30} color="white" />
-                <TouchableOpacity style={styles.publicarDiv} onPress={() => postTopic()} >
-                    <MaterialIcons name="share" size={30} color="white" style={{marginRight:10}}/>
-                </TouchableOpacity> 
-            </View>
-    )
-};
-const styles = StyleSheet.create({
-    header: {
-        flexDirection: 'row',
-        width:largura,
-        alignItems:'flex-start',
-        justifyContent:'space-between',
-        padding:20,
-        flex:1,
-        marginBottom:10,   
-    },
-    publicarDiv:{
-        flexDirection:"row-reverse",
-        alignSelf:'flex-start',
-        flex:0.2
-    },
-    publicarText:{
-        marginTop:2.5,
-        marginRight:20,
-        color:'white',
-        fontSize:15,
-        fontWeight: '100',
-    },
-    backButton:{
-        padding:10
-    }
-});
->>>>>>> 18c6322 (adding topic view)
