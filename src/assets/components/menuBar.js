20cf0558e87a6b044ee42563c3bd1fef83fcd037
Marcos Felipe <marofelipe@live.com>
2020-11-25 02:03:51 -0300

    Adding Forum view and navigation betwween viewws

--- src/assets/components/menuBar.js
+++ src/assets/components/menuBar.js
@@ -1,5 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
 import { View } from 'react-native';
 import { Feather, AntDesign, Ionicons, Entypo } from '@expo/vector-icons';
 import styles from './style';
@@ -50,33 +49,3 @@ export default function MenuBar({ navigation }) {
     </View>
   );
 }
-=======
-import { View, Text } from 'react-native';
-import { Feather, AntDesign, Ionicons, Entypo } from '@expo/vector-icons';
-import styles from './style';
- export default function MenuBar({ navigation }){
-     return(
-         <View style={styles.mbContainer}>
-             <View style={styles.mbContent}>
-                <View style={styles.mbIcons}>
-                    <Feather style={styles.mbIcons} name="home" size={32} color="white" onPress={() =>navigation.push('Explore')}/>
-                </View>
-                <View style={styles.mbIcons}>
-                    <Entypo name="flower" size={30} color="white" />
-                </View>
-                <View style={styles.mbCameraDiv}>
-                    <View style={styles.mbCameraIcon}>
-                        <Feather name="camera" size={35} color="white" onPress={() =>navigation.push('Scanner')}/>
-                    </View>
-                </View>
-                <View style={styles.mbIcons}>
-                    <Ionicons name="logo-twitch" size={32} color="white" onPress={() =>navigation.push('Forum')} />
-                </View>
-                <View style={styles.mbIcons}>
-                    <AntDesign name="user" size={32} color="white" />
-                </View>
-             </View>
-         </View>
-     )
- };
->>>>>>> 7587ca1 (Adding Forum view and navigation betwween viewws)
