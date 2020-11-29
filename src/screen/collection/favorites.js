7ec6b019e6786e5e3e5997eadf4929f08e67da8c
Victor Samuel <victor.samuelsantoss@gmail.com>
2020-11-28 23:14:09 -0300

    Creating favorites collection
    Co-authored-by: Marcos Felipe <marolipe@live.com>

--- src/screen/collection/favorites.js
+++ src/screen/collection/favorites.js
@@ -1,7 +1,7 @@
 import React ,{ useState, useEffect }from 'react';
-import { View, Text, FlatList } from 'react-native';
+import { View, Text, FlatList, TouchableOpacity, ImageBackground } from 'react-native';
 import styles from './style';
-import { AntDesign, EvilIcons } from '@expo/vector-icons'
+import { AntDesign, EvilIcons, Entypo } from '@expo/vector-icons'
 
 export default function Favorite({ navigation }){
     const[favorites,setFavorites] = useState({});
@@ -16,13 +16,34 @@ export default function Favorite({ navigation }){
                 <View style={styles.headerIcon}>
                     <AntDesign name="left" size={30} color="black" />
                 </View>
-                <View style={styles.headerIcon}>
+                <View style={styles.headerTitle}>
                     <Text style={styles.headerTitleText}>Favoritos</Text>
-                    <EvilIcons name="search" size={35} color="black" />
+                    <EvilIcons name="search" size={50} color="black" />
                 </View>
             </View>
             <View style={styles.body}>
-            
+                <View style={styles.plant}>
+                    <View style={styles.plantBackground}>
+                   
+                    </View>
+
+                    <View style={styles.plantDescription}>
+                        <Text style={styles.plantDescriptionTitle}>
+                            Samambaia
+                        </Text>
+                        <Text style={styles.plantDescriptionText}>
+                            Uma planta cheia de Ritmo
+                        </Text>
+
+                        <View style={styles.plantButton}>
+                        <Entypo name="chevron-right" size={24} color="white" />
+                        </View>
+                    </View>
+
+                    
+
+
+                </View>
             </View>
         </View>
         
--- src/screen/collection/style.js
+++ src/screen/collection/style.js
@@ -6,27 +6,79 @@ const styles = StyleSheet.create({
  container:{
      flex:1,
      flexDirection:'column',
-     backgroundColor:'black'
  },
  header:{
-     flex:0.2,
+     flex: 0.25,
      flexDirection:'column',
-     backgroundColor:'blue'
+     width: largura/1.1,
+     alignSelf: 'center',
  },
  headerIcon:{
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
     alignSelf:'flex-start',
+    flex: 0.6
  },
  headerTitle:{
+    width: largura/1.2,
     flexDirection:'row',
-    justifyContent:'space-between'
+    alignItems: 'center',
+    justifyContent:'space-between',
  },
  headerTitleText:{
-    fontSize:30,
-    fontWeight:'600'
+    fontSize: 35,
+    fontWeight:'600',
+    paddingLeft: 5,
  },
  body:{
-     flex:0.8,
-     backgroundColor:'blue'
+    flex:0.7,
+    alignItems: 'center',
+    paddingBottom: 20,
+ },
+ plant:{
+    flex: 0.45,
+    width: largura/1.2,
+    flexDirection: 'row'
+ },
+ plantBackground: {
+    flex: 1,
+    backgroundColor: 'green',
+ }, 
+ plantDescription:{
+    flex: 1,
+    backgroundColor: '#59A44D',
+    fontWeight: '600',
+    height: '40%',
+    alignSelf: 'center',
+    marginTop: '25%',
+    borderTopRightRadius: 30,
+    borderBottomRightRadius: 30,
+    flexDirection: 'column',
+    alignItems: 'center',
+    padding: 10,
+ },
+ plantDescriptionTitle: {
+    fontSize: 17,
+    color:'white',
+    paddingBottom: 5,
+    textAlign: 'center'
+ },
+ plantDescriptionText:{
+    fontSize: 15,
+    color: 'white',
+    textAlign: 'center'
+ },
+ plantButton: {
+    width: '45%',
+    height: '45%',
+    backgroundColor: 'orange',
+    alignSelf: 'flex-start',
+    margin: 5,
+    marginTop: '10%',
+    borderRadius: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
  }
 });
 export default styles;
