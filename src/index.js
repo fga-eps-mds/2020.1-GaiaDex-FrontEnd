781c43af8e2685fefacd74d5111f18bdd5e5b1c7
Rafael Makaha <rafael.makaha@gmail.com>
2020-12-02 17:49:44 -0300

    Remove unused files and imports.
    Signed-off-by: Rafael Makaha <rafael.makaha@gmail.com>
    Co-authored-by: João Vítor Morandi <joao.lemos4000@gmail.com>
    Co-authored-by: Marcos Filipe <marofelipe@live.com>

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
 import React from 'react';
 import Routes from './Routes';
-import Login from './screen/login/login';
-import PlantView from './screen/plant/plantView';
-import Scanner from './screen/scanner';
-import Signup from './screen/login/signup'
 
 export default function App() {
   return <Routes />;
