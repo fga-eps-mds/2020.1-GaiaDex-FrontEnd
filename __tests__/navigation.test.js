b55011b2744ea6963a5bf3b0730aeb86e1768967
Marcos Gabriel Tavares <marcosgabriel.ro@hotmail.com>
2020-12-10 00:01:01 -0400

    test navigation from login to signup and vice versa

---
+++ __tests__/navigation.test.js
@@ -0,0 +1,69 @@
+import 'react-native-gesture-handler/jestSetup';
+
+jest.mock('react-native-reanimated', () => {
+  const Reanimated = require('react-native-reanimated/mock');
+
+  // The mock for `call` immediately calls the callback which is incorrect
+  // So we override it with a no-op
+  Reanimated.default.call = () => {};
+
+  return Reanimated;
+});
+
+// Silence the warning: Animated: `useNativeDriver` is not supported because the native animated module is missing
+jest.mock('react-native/Libraries/Animated/src/NativeAnimatedHelper');
+
+import mockAsyncStorage from '@react-native-async-storage/async-storage/jest/async-storage-mock';
+
+jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage);
+
+import { NavigationContainer } from '@react-navigation/native';
+import { render, fireEvent } from '@testing-library/react-native';
+
+import Routes from '../src/Routes';
+
+
+function Test(){
+  return(
+    <NavigationContainer>
+        <Routes />
+    </NavigationContainer>
+  );
+}
+
+
+describe('<AuthStack />', () => {
+
+  it('navigates from login to signup and from signup to login', async() => {
+    const component = (
+      <NavigationContainer>
+        <Routes />
+      </NavigationContainer>
+    );
+
+    const { findByText, findAllByText } = render(component);
+
+    var log = await findByText('Seja bem-vindo');
+
+    expect(log).toBeTruthy();
+
+    var toClick = await findByText('Cadastro');
+    fireEvent(toClick, 'press');
+
+    const cad = await findByText('Criar Conta');
+
+    expect(cad).toBeTruthy();
+
+    var toClick = await findAllByText('Login');
+    fireEvent(toClick[1], 'press');
+
+    var log = await findAllByText('Seja bem-vindo');
+
+    expect(log[1]).toBeTruthy();
+
+
+  });
+
+ 
+
+});
