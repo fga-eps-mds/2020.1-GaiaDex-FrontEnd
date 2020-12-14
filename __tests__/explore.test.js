a3be517aa8415e396a22371239fd16c427ec517b
Marcos Gabriel Tavares <marcosgabriel.ro@hotmail.com>
2020-12-13 21:15:32 -0400

    added explore screen test Co-authored-by: Victor Samuel dos Santos Lucas <victor.samuelsantoss@gmail.com> Co-authored-by: Vinícius Vieira de Souza <vd.souza1999@gmail.com>

---
+++ __tests__/explore.test.js
@@ -0,0 +1,61 @@
+import mockAsyncStorage from '@react-native-async-storage/async-storage/jest/async-storage-mock';
+
+jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage);
+
+import Explore from '../src/screen/explore/explore';
+import styles from '../src/screen/explore/styles';
+
+
+
+
+const wrapper = mount(<Explore />);
+
+describe('<Explore />', () => {
+  it('renders everything', () => {
+    expect(wrapper).toMatchSnapshot;
+    expect(wrapper.find('View').length).toBe(34);
+    expect(wrapper.find('TouchableOpacity').length).toBe(1);
+    expect(wrapper.find('Icon').length).toBe(6);
+    expect(wrapper.find('Text').length).toBe(18);
+    expect(wrapper.find('FlatList').length).toBe(2);
+    expect(wrapper.find('MenuBar').length).toBe(1);
+  });
+  it('renders View correctly', () => {
+    expect(wrapper.find('View').at(0)).toHaveStyle(styles.container);
+    expect(wrapper.find('View').at(2)).toHaveStyle(styles.exploreContainer);
+    expect(wrapper.find('View').at(4)).toHaveStyle(styles.popularContainer);
+    expect(wrapper.find('View').at(6)).toHaveStyle(styles.popularTabs);
+    expect(wrapper.find('View').at(8)).toHaveStyle(styles.popularContent);
+    expect(wrapper.find('View').at(12)).toHaveStyle(styles.myPlantsContainer);
+
+  });
+  it('renders TouchableOpacity correctly', () => {
+    expect(wrapper.find('TouchableOpacity').at(0)).toHaveProp('onPress');
+
+  });
+  it('renders Icon correctly', () => {
+    expect(wrapper.find('Icon').at(0)).toHaveProp('name','search');
+    expect(wrapper.find('Icon').at(0)).toHaveProp('size', 45);
+    expect(wrapper.find('Icon').at(0)).toHaveProp('color','black');
+  });
+  it('renders Text correctly', () => {
+    expect(wrapper.find('Text').at(0)).toHaveStyle(styles.exploreText);
+    expect(wrapper.find('Text').at(0)).toHaveText('Explorar');
+    expect(wrapper.find('Text').at(4)).toHaveStyle(styles.popularTab);
+    expect(wrapper.find('Text').at(4)).toHaveText('Populares');
+    expect(wrapper.find('Text').at(6)).toHaveStyle(styles.popularTab);
+    expect(wrapper.find('Text').at(6)).toHaveText('Minhas Plantas');
+  });
+  it('renders FlatList correctly', () => {
+    expect(wrapper.find('FlatList').at(0)).toHaveProp('horizontal');
+    expect(wrapper.find('FlatList').at(0)).toHaveProp('renderItem');
+    expect(wrapper.find('FlatList').at(0)).toHaveProp('data');
+    expect(wrapper.find('FlatList').at(0)).toHaveProp('keyExtractor');
+
+  });
+  it('renders MenuBar correctly', () => {
+    expect(wrapper.find('MenuBar').at(0)).toHaveProp('navigation');
+
+  });
+  
+});
