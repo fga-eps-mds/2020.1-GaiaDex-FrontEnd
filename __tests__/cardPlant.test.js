5890c5b13d681cd1a9d263638f9ed4c2ab6c75d0
Marcos Gabriel Tavares <marcosgabriel.ro@hotmail.com>
2020-12-13 20:24:51 -0400

    fixed cardPlant test Co-authored-by: Victor Samuel dos Santos Lucas <victor.samuelsantoss@gmail.com> Co-authored-by: Vinícius Vieira de Souza <vd.souza1999@gmail.com>

--- __tests__/cardPlant.test.js
+++ __tests__/cardPlant.test.js
@@ -13,8 +13,8 @@ describe('<TopicCreate />', () => {
     expect(wrapper.find('View').length).toBe(40);
     expect(wrapper.find('Header').length).toBe(1);
     expect(wrapper.find('ImageBackground').length).toBe(1);
-    expect(wrapper.find('Text').length).toBe(36);
-    expect(wrapper.find('TouchableOpacity').length).toBe(2);
+    expect(wrapper.find('Text').length).toBe(30);
+    expect(wrapper.find('TouchableOpacity').length).toBe(3);
     expect(wrapper.find('PlantCardTopic').length).toBe(1);
     expect(wrapper.find('NewTopic').length).toBe(1);
   });
@@ -32,13 +32,19 @@ describe('<TopicCreate />', () => {
     expect(wrapper.find('Header').at(0)).toHaveProp('navigation');
   });
   it('renders Text correctly', () => {
-    expect(wrapper.find('Text').at(8)).toHaveStyle(styles.plantInfoTitle);
-    expect(wrapper.find('Text').at(12)).toHaveStyle(styles.plantInfoText);
-    expect(wrapper.find('Text').at(16)).toHaveStyle(styles.plantInfoText);
-    expect(wrapper.find('Text').at(20)).toHaveStyle(
-      styles.plantTextDescription
-    );
-    expect(wrapper.find('Text').at(22)).toHaveStyle(styles.menuBarTabText);
+    expect(wrapper.find('Text').at(2)).toHaveStyle(styles.plantInfoTitle);
+    expect(wrapper.find('Text').at(6)).toHaveStyle(styles.plantInfoText);
+    expect(wrapper.find('Text').at(6)).toHaveText(' Usuários');
+    expect(wrapper.find('Text').at(10)).toHaveStyle(styles.plantInfoText);
+    expect(wrapper.find('Text').at(10)).toHaveText(' Tópicos');
+
+    expect(wrapper.find('Text').at(14)).toHaveStyle(styles.plantTextDescription);
+    expect(wrapper.find('Text').at(14)).toHaveText('Esta é uma comunidade certificada/padrão do app. Direcionada para se interagir sobre quaisquer assuntos envolvendo cenouras.');
+
+    expect(wrapper.find('Text').at(16)).toHaveStyle(styles.menuBarTabText);
+    expect(wrapper.find('Text').at(16)).toHaveText('Informações');
+    expect(wrapper.find('Text').at(20)).toHaveStyle(styles.menuBarTabText);
+    expect(wrapper.find('Text').at(20)).toHaveText('Topicos');
   });
   it('renders TouchableOpacity correctly', () => {
     expect(wrapper.find('TouchableOpacity').at(0)).toHaveStyle(
