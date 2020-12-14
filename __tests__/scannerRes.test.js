4c2ac618c1498a9e484f8328e94525353e73800d
Marcos Gabriel Tavares <marcosgabriel.ro@hotmail.com>
2020-12-13 20:41:56 -0400

    fixed scannerRes test Co-authored-by: Victor Samuel dos Santos Lucas <victor.samuelsantoss@gmail.com> Co-authored-by: Vinícius Vieira de Souza <vd.souza1999@gmail.com>

--- __tests__/scannerRes.test.js
+++ __tests__/scannerRes.test.js
@@ -3,7 +3,9 @@ import mockAsyncStorage from '@react-native-async-storage/async-storage/jest/asy
 jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage);
 
 import result from '../src/screen/scanner/result';
-import styles from '../src/screen/scanner/styles'
+import styles from '../src/screen/scanner/styles';
+import stylesEdit from '../src/screen/collection/styles';
+
 
 const Sresult = result;
 
@@ -13,11 +15,11 @@ const wrapper = mount(<Sresult />);
 describe('<Scamera />', () => {
   it('renders everything', () => {
     expect(wrapper).toMatchSnapshot;
-    expect(wrapper.find('View').length).toBe(6);
+    expect(wrapper.find('View').length).toBe(16);
     expect(wrapper.find('Image').length).toBe(2);
-    expect(wrapper.find('TouchableOpacity').length).toBe(1);
+    expect(wrapper.find('TouchableOpacity').length).toBe(3);
     expect(wrapper.find('Icon').length).toBe(1);
-    expect(wrapper.find('Text').length).toBe(4);
+    expect(wrapper.find('Text').length).toBe(10);
     expect(wrapper.find('FlatList').length).toBe(1);
 
   });
@@ -31,12 +33,19 @@ describe('<Scamera />', () => {
 
   });
   it('renders TouchableOpacity correctly', () => {
-    expect(wrapper.find('TouchableOpacity').at(0)).toHaveStyle({ margin: 10 });
     expect(wrapper.find('TouchableOpacity').at(0)).toHaveProp('onPress');
-
+    expect(wrapper.find('TouchableOpacity').at(1)).toHaveProp('onPress');
+    expect(wrapper.find('TouchableOpacity').at(2)).toHaveProp('onPress');
   });
   it('renders Text correctly', () => {
-    expect(wrapper.find('Text').at(2)).toHaveText('Esses são os resultados para a sua foto:');
+    expect(wrapper.find('Text').at(0)).toHaveStyle(stylesEdit.editTitle);
+    expect(wrapper.find('Text').at(0)).toHaveText('Digite o novo nome para sua planta:');
+    expect(wrapper.find('Text').at(2)).toHaveStyle(stylesEdit.editButton);
+    expect(wrapper.find('Text').at(2)).toHaveText('FEITO');
+    expect(wrapper.find('Text').at(4)).toHaveStyle(stylesEdit.editButton);
+    expect(wrapper.find('Text').at(4)).toHaveText('CANCELAR');
+    expect(wrapper.find('Text').at(8)).toHaveStyle(styles.titulo);
+    expect(wrapper.find('Text').at(8)).toHaveText('Esses são os resultados para a sua foto:');
 
   });
   it('renders FlatList correctly', () => {
